79476d4c23f21b6e3b079bf0660bd0397b80c573
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-11-28 13:36:35 -0300

    Update script-bd.sql

--- script-bd.sql
+++ script-bd.sql
@@ -1,16 +1,18 @@
+CREATE TABLE plano (
+	id_plano SERIAL PRIMARY KEY,
+	nome_plano VARCHAR(50) UNIQUE NOT NULL,
+	valor_plano FLOAT NOT NULL,
+	descricao_plano VARCHAR(100) NOT NULL,
+	id_plano INT,
+	FOREIGN KEY (id_plano) REFERENCES plano(id_plano)
+	)
+
 CREATE TABLE usuario (
 	id_usuario SERIAL PRIMARY KEY,
 	cpf_usuario VARCHAR(50) UNIQUE NOT NULL,
 	nome_usuario VARCHAR(100) NOT NULL,
 	senha VARCHAR(50) NOT NULL
 )
-
-CREATE TABLE plano (
-	id_plano SERIAL PRIMARY KEY,
-	nome_plano VARCHAR(50) UNIQUE NOT NULL,
-	valor_plano FLOAT NOT NULL,
-	descricao_plano VARCHAR(100) NOT NULL
-	)
 
 INSERT INTO plano (nome_plano, valor_plano, descricao_plano)
 VALUES ('mensal',90,'1 mês, Sem Desconto')
