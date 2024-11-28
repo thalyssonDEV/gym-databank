8a0f51510bec41aa4c25da9dea7e553ce15a8786
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-11-28 15:53:12 -0300

    Create insertions-script-bd.sql

---
+++ insertions-script-bd.sql
@@ -0,0 +1,15 @@
+INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
+VALUES ('mensal',90,30,'1 mês, Sem Desconto');
+
+INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
+VALUES ('bimestral',140,60,'2 meses, 22% OFF');
+
+INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
+VALUES ('trimestral',175.50,90,'3 meses, 35% OFF');
+  
+INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
+VALUES ('semestral',324,180,'6 meses, 40% OFF');
+
+INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
+VALUES ('anual',540,360,'1 ano, 50% OFF');
+
