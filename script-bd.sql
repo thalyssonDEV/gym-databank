bc7a5799f41d1b269eeb3345de746cf5ec56058a
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-11-28 13:48:14 -0300

    Update script-bd.sql

--- script-bd.sql
+++ script-bd.sql
@@ -2,29 +2,29 @@ CREATE TABLE plano (
 	id_plano SERIAL PRIMARY KEY,
 	nome_plano VARCHAR(50) UNIQUE NOT NULL,
 	valor_plano FLOAT NOT NULL,
-	descricao_plano VARCHAR(100) NOT NULL,
-	id_plano INT,
-	FOREIGN KEY (id_plano) REFERENCES plano(id_plano)
-	)
+	descricao_plano VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE usuario (
 	id_usuario SERIAL PRIMARY KEY,
 	cpf_usuario VARCHAR(50) UNIQUE NOT NULL,
 	nome_usuario VARCHAR(100) NOT NULL,
-	senha VARCHAR(50) NOT NULL
-)
+	senha VARCHAR(50) NOT NULL,
+	id_plano INT,
+	FOREIGN KEY (id_plano) REFERENCES plano(id_plano)
+);
 
 INSERT INTO plano (nome_plano, valor_plano, descricao_plano)
-VALUES ('mensal',90,'1 mês, Sem Desconto')
+VALUES ('mensal',90,'1 mês, Sem Desconto');
 
 INSERT INTO plano (nome_plano, valor_plano, descricao_plano)
-VALUES ('bimestral',140,'2 meses, 22% OFF')
+VALUES ('bimestral',140,'2 meses, 22% OFF');
 
 INSERT INTO plano (nome_plano, valor_plano, descricao_plano)
-VALUES ('trimestral',175.50,'3 meses, 35% OFF')
+VALUES ('trimestral',175.50,'3 meses, 35% OFF');
   
 INSERT INTO plano (nome_plano, valor_plano, descricao_plano)
-VALUES ('semestral',324,'6 meses, 40% OFF')
+VALUES ('semestral',324,'6 meses, 40% OFF');
 
 INSERT INTO plano (nome_plano, valor_plano, descricao_plano)
-VALUES ('anual',540,'1 ano, 50% OFF')
+VALUES ('anual',540,'1 ano, 50% OFF');
