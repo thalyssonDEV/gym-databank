a1ebb699863f32fdc8ee81442c83130a5a349845
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-11-28 15:52:51 -0300

    Update script-bd.sql

--- script-bd.sql
+++ script-bd.sql
@@ -17,21 +17,6 @@ CREATE TABLE usuario (
 	data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
-VALUES ('mensal',90,30,'1 mês, Sem Desconto');
-
-INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
-VALUES ('bimestral',140,60,'2 meses, 22% OFF');
-
-INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
-VALUES ('trimestral',175.50,90,'3 meses, 35% OFF');
-  
-INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
-VALUES ('semestral',324,180,'6 meses, 40% OFF');
-
-INSERT INTO plano (nome_plano, valor_plano, duracao_dias, descricao_plano)
-VALUES ('anual',540,360,'1 ano, 50% OFF');
-
 CREATE TABLE grupo_muscular (
 	id_grupo SERIAL PRIMARY KEY,
 	nome_grupo VARCHAR(100) NOT NULL
